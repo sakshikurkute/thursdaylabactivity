e02de9a009156172717dffcdbf88bed4157aaa08
Sakshi <Sakshi@DESKTOP-PIQGTMN>
2023-02-11 11:23:38 +0530

    activity1

---
+++ activity01/src/activity01/numberpattern.java
@@ -0,0 +1,26 @@
+package activity01;
+
+import java.util.Scanner;
+
+public class numberpattern {
+
+
+		public static void main(String[] args) {
+
+		int i, j, k = 1;
+
+		for (i = 1; i <= 5; i++) {
+
+		for (j = 1; j < i + 1; j++) {
+
+		System.out.print(k++ + " "); 
+
+		}
+
+		System.out.println();
+
+		}
+
+		}
+
+}
